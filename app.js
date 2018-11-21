9f5dddba87b6bb339b8e2e52aa44431c7face3e1
Joshua Wheeler <jwheeler@tvpage.com>
2018-11-20 18:27:52 -0800

    Template interpolating data

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var Handlebars = require('handlebars');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+const port = 3000;
+
+
+/****** HELPER METHODS *******/
+
+
+
+/****** API ENDPOINTS ********/
+const app = express();
+
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.put('/api/render/*', function (req, res) {
+    const page_data = req.body;
+    const template_path = req.params[0];
+
+    fs.readFile('./templates/' + template_path, {encoding: 'utf-8'}, function(err, contents) {
+        var template = Handlebars.compile(contents);
+        var result = template(page_data);
+        res.send(result);
+    });
+});
+
+app.get('');
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
